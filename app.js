37c1a752f5ddd3b9c58ca8f5d0f48237ac513ba4
João Victor de F. Lima <jvdfreitas@gmail.com>
2024-09-06 11:07:17 -0300

    Subindo a ultima versão do projeto Gemini

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 function pesquisar() {
   // Obtém a seção HTML onde os resultados serão exibidos
   let section = document.getElementById("resultados-pesquisa");
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
   // Inicializa uma string vazia para armazenar os resultados formatados
   let resultado = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
 
   // Itera sobre cada item de dados (assumindo que 'dados' é um array de objetos)
   for (let dado of dados) {
-    // Cria um novo elemento div para cada resultado, formatando-o como HTML
-    resultado += `<div class="item-resultado">
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+    if (!campoPesquisa) {
+      section.innerHTML =
+        "Nada foi encontrado. Digite um nome de um(a) atleta ou esporte.";
+      return;
+    } else if (
+      titulo.includes(campoPesquisa) ||
+      descricao.includes(campoPesquisa) ||
+      tags.includes(campoPesquisa)
+    ) {
+      resultado += `<div class="item-resultado">
       <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
       <p class="descrisao-meta">${dado.descricao}</p>
       <a href=${dado.link} target="_blank">Veja mais.</a>
     </div>`;
+    } else if (!resultado) {
+      section.innerHTML = "Nada foi encontrado.";
+      return;
+    }
+    // Cria um novo elemento div para cada resultado, formatando-o como HTML
   }
 
   // Atribui o HTML gerado à seção de resultados
